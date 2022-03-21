9ad7b6d6d3d2a681d2ff47e2864a5649e3469ff1
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-03-21 19:56:29 +0530

    Time: 3 ms (51.45%), Space: 51.7 MB (40.52%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -1,27 +1,27 @@
 class Solution {
     public void solve(char[][] board) {
-           int n=board.length;
-        int m=board[0].length;
+        int n = board.length;
+        int m = board[0].length;
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(i==0|| j==0|| i==n-1 || j==m-1){
-                    if(board[i][j]=='O'){
-                        // this island is not surrounded, convert to '$'
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(i==0 || j==0 || i == n-1 || j == m-1) {
+                    if(board[i][j] == 'O') {
                         dfs(i,j,board,n,m);
                     }
                 }
             }
         }
         
-        for(int i=0; i<n; i++){
-            for(int j=0;j<m; j++){
-                if(board[i][j]=='O'){ // surrounded
-                    board[i][j]='X';
-                } else if(board[i][j]=='$'){ // not surrounded
-                    board[i][j]='O';
+        
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                    if(board[i][j] == 'O') {
+                        board[i][j] = 'X';
+                    } else if(board[i][j] == '$') {
+                        board[i][j] = 'O';
+                    }
                 }
-            }
         }
     }
     
