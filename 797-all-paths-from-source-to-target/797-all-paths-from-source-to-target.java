cc262a9145458979afea3590905587e5d79958d9
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-03-23 21:58:43 +0530

    Time: 3 ms (85.05%), Space: 55.8 MB (10.11%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -0,0 +1,32 @@
+class Solution {
+     List<List<Integer>> paths;
+	 List<Integer> path;
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        paths = new ArrayList<>();
+	 	path = new ArrayList<>();
+        int n = graph.length;
+        boolean[] vis = new boolean[n];
+        path.add(0);
+        allPathsFromSourceTarget(0,graph.length-1,vis,graph);
+        return paths;
+    }
+    public void allPathsFromSourceTarget(int src,int dest,boolean[] vis,int[][] graph) {
+        if(src == dest) {
+            paths.add(new ArrayList<>(path));
+            return;
+        }
+        vis[src] = true;
+        
+        
+        for(int nbr : graph[src]) {
+            if(!vis[nbr]) {
+                path.add(nbr);
+                
+                allPathsFromSourceTarget(nbr,dest,vis,graph);
+                 
+                path.remove(path.size()-1);
+            }
+        }
+        vis[src] = false;
+    }
+}
