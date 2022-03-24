963a6f24dee1a7fc72f5922ed2fc17b4b9e5d47e
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-03-24 22:24:26 +0530

    Time: 7 ms (72.50%), Space: 64.2 MB (61.06%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,62 @@
+class Solution {
+    int[] par;
+    int[] size;
+    public int findParent(int u) {
+        if(par[u] == u) 
+            return u;
+        
+        int p = findParent(par[u]);
+        
+        par[u] = p;
+        
+        return p;
+    }
+    
+    public void merge(int p1,int p2) {
+        if(size[p1] >= size[p2]) {
+            par[p2] = p1;
+            size[p1] += size[p2];
+        } else {
+            par[p1] = p2;
+            size[p2] += size[p1];
+        }
+    }
+    
+    public int makeConnected(int n, int[][] connections) {
+        if(connections.length < n - 1) {
+            // impossible to connect all n vertices/computers
+            return -1;
+        }
+        
+        int cn = n;
+        int numberOfConnections = 0;
+        
+        par = new int[cn];
+        size = new int[cn];
+        
+        //initializing parent array
+        for(int i=0;i<cn;i++) {
+            par[i] = i;
+            size[i] = 1;
+        }
+        
+        for(int i=0;i<connections.length;i++) {
+            int u = connections[i][0];
+            int v = connections[i][1];
+            
+            int lu = findParent(u);
+            int lv = findParent(v);
+            
+            if(lu != lv) {
+                merge(lu,lv);
+            } 
+        }
+        
+        for(int i=0;i<par.length;i++) {
+            if(par[i] == i)
+                numberOfConnections++;
+        }
+        
+       return numberOfConnections-1;
+    }
+}
