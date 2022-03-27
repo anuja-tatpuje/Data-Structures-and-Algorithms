a8aa14128cbd7c002e5a3a6762b34af53b860ee2
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-03-27 12:40:36 +0530

    Time: 6 ms (37.24%), Space: 49.8 MB (97.31%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -8,7 +8,7 @@ class Solution {
        for(int i=0;i<n;i++) {
            for(int j=0;j<m;j++) {
                if(grid[i][j] == '1') {
-                 dfs(i,j,grid);
+                 bfs(i,j,grid);
                    count++;   
                }
            }
@@ -33,4 +33,34 @@ class Solution {
              }
          }
     }
+    
+    public void bfs(int row,int col,char[][] grid) {
+        int m = grid[0].length;
+        int n = grid.length;
+        LinkedList<Integer> que = new LinkedList<>();
+        
+        que.addLast(row*m+col);
+         grid[row][col] = '0';
+        
+        while(que.size() > 0) {
+            int top = que.removeFirst();
+              
+            int x = top / m;
+            int y = top % m;
+            
+          
+            
+          int[][] dirs={{0,1},{0,-1},{1,0},{-1,0}};
+        
+          for(int[] dir : dirs) {
+             int r = x + dir[0];
+             int c = y + dir[1];
+             
+             if(r>=0 && c>=0 && r < n  && c < m && grid[r][c] == '1') {
+                que.addLast(r*m+c);
+                grid[r][c] = '0';
+             }
+        }
+    }
+  } 
 }
