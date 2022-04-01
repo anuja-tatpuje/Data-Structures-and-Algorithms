63e72f0a96c8abba9848f3ea2dd7e56ad03bddf3
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-04-01 22:21:34 +0530

    Time: 2 ms (33.86%), Space: 57.6 MB (29.35%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int st = 0;
+        int end = s.length-1;
+        
+        while(st<=end) {
+            char temp = s[st];
+            s[st] = s[end];
+            s[end] = temp;
+            st++;
+            end--;
+        }
+    }
+}
