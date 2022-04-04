6dd7ea0628366a7918437f90d676539dab3552cb
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-04-04 22:02:38 +0530

    Time: 4 ms (39.21%), Space: 41.8 MB (42.71%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.java
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -2,11 +2,39 @@ class Solution {
   List<String> list = new ArrayList<>();
     String ans = "";
     int count = 0;
+    public void getPermutations(int n,int k) {
+        //calculate factorial
+        int fact = 1;
+        List<Integer> fa = new ArrayList<>();
+        
+        for(int i=1;i<n;i++) {
+            fact = fact * i;
+            fa.add(i);
+        }
+        fa.add(n);
+        
+        //reducing k as we have 0 based indexing
+         k -= 1;
+        while(true) {
+            ans += fa.get(k/fact);
+            fa.remove(k/fact);
+            if(fa.size() == 0) 
+                break;
+            
+            //calculating which index element to take
+            k = k % fact;
+            fact = fact / fa.size();
+        }
+
+    }
     public String getPermutation(int n, int k) {
-        boolean[] vis = new boolean[n+1];
+      /*  boolean[] vis = new boolean[n+1];
         StringBuilder s = new StringBuilder();
         count = k;
         permutationSequence(n,vis,s);
+        return ans; */
+        
+        getPermutations(n,k);
         return ans;
     }
     public void permutationSequence(int n,boolean[] vis,StringBuilder s) {
