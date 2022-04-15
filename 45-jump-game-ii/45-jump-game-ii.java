5d1d056f481b68ca79f9466c93a477bbdbd66587
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-04-15 17:22:41 +0530

    Time: 48 ms (35.27%), Space: 43 MB (82.49%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        int ans = jumpRec(n,nums,0,dp);
+        int ans = jumpTab(n,nums);
         return ans;
     }
     public int jumpRec(int n,int[] nums,int idx,int[] dp) {
@@ -19,5 +19,26 @@ class Solution {
         }
         
         return dp[idx] = ans+1;
+    }
+    public int jumpTab(int n,int[] nums) {
+        int[] dp = new int[n+1];
+        
+        for(int idx = n-1;idx>=0;idx--) {
+            if(idx == n-1) {
+                dp[idx] = 0;
+                continue;
+            }
+
+            if(dp[idx] != 0) return dp[idx];
+            int ans = (int)(1e8);
+            int jumpsAllowed = nums[idx];
+
+            for(int j=1;j<=jumpsAllowed && j+idx<n;j++) {
+               ans = Math.min(ans,dp[j+idx]);
+            }
+            
+            dp[idx] = ans+1;
+        }
+         return dp[0];
     }
 }
