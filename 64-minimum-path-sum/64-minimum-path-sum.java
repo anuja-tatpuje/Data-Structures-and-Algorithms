7b8750dc8ad5fe2cdb35436b6cbabd29cc1ac43f
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-04-16 18:08:46 +0530

    Time: 1 ms (99.54%), Space: 45.2 MB (78.76%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -9,34 +9,32 @@ class Solution {
         
     }
     public int recMinPathSum(int i,int j,int n,int m,int[][] grid) {
-        if(i==n-1 && j==m-1) return grid[i][j];
-        
+        if(i==n-1 && j==m-1) {
+            return grid[i][j];
+        }
         int ans = (int) 1e8;
-        
         if(i+1<n) {
             ans = Math.min(ans,recMinPathSum(i+1,j,n,m,grid));
-        } 
+        }
         if(j+1<m) {
-            ans = Math.min(ans,recMinPathSum(i,j+1,n,m,grid));
+             ans = Math.min(ans,recMinPathSum(i,j+1,n,m,grid));
         }
         
         return ans + grid[i][j];
     }
-    public int recMinPathSumMemo(int i, int j,int n,int m,int[][] grid,int[][] dp) {
-        if(i == n-1 && j==m-1)  {
+    public int recMinPathSumMemo(int i,int j,int n,int m,int[][] grid,int[][] dp) {
+        if(i==n-1 && j==m-1) {
             return dp[i][j] = grid[i][j];
         }
         
         if(dp[i][j] != 0) return dp[i][j];
         
         int ans = (int) 1e8;
-        
         if(i+1<n) {
             ans = Math.min(ans,recMinPathSumMemo(i+1,j,n,m,grid,dp));
         }
-        
         if(j+1<m) {
-            ans = Math.min(ans,recMinPathSumMemo(i,j+1,n,m,grid,dp));
+             ans = Math.min(ans,recMinPathSumMemo(i,j+1,n,m,grid,dp));
         }
         
         return dp[i][j] = ans + grid[i][j];
