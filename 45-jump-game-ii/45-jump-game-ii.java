27c621b358d10141a382b76db93f5ede56f7f4cd
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-04-16 17:42:28 +0530

    Time: 171 ms (14.51%), Space: 51.2 MB (5.22%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -2,43 +2,37 @@ class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        int ans = jumpTab(n,nums);
+        int ans = jumpMemoi(nums,n,0,dp);
         return ans;
     }
-    public int jumpRec(int n,int[] nums,int idx,int[] dp) {
-        if(idx == n-1) {
-            return dp[idx] = 0;
+    public int jumpRec(int[] nums,int n,int idx) {
+        if(idx==n-1) {
+            return 0;    
+        }
+        
+        int ans = (int) (1e8);
+        int jumps_allowed = nums[idx];
+        
+        for(int j=1;j<=jumps_allowed && j+idx<n;j++) {
+           ans = Math.min(ans,jumpRec(nums,n,j+idx));
+        }
+        
+        return ans+1;
+    }
+    public int jumpMemoi(int[] nums,int n,int idx,int[] dp) {
+        if(idx==n-1) {
+            return dp[idx] = 0;   
         }
         
         if(dp[idx] != 0) return dp[idx];
-        int ans = (int)(1e8);
-        int jumpsAllowed = nums[idx];
         
-        for(int j=1;j<=jumpsAllowed && j+idx<n;j++) {
-           ans = Math.min(ans,jumpRec(n,nums,j+idx,dp));
+        int ans = (int) (1e8);
+        int jumps_allowed = nums[idx];
+        
+        for(int j=1;j<=jumps_allowed && j+idx<n;j++) {
+           ans = Math.min(ans,jumpMemoi(nums,n,j+idx,dp));
         }
         
         return dp[idx] = ans+1;
-    }
-    public int jumpTab(int n,int[] nums) {
-        int[] dp = new int[n+1];
-        
-        for(int idx = n-1;idx>=0;idx--) {
-            if(idx == n-1) {
-                dp[idx] = 0;
-                continue;
-            }
-
-            if(dp[idx] != 0) return dp[idx];
-            int ans = (int)(1e8);
-            int jumpsAllowed = nums[idx];
-
-            for(int j=1;j<=jumpsAllowed && j+idx<n;j++) {
-               ans = Math.min(ans,dp[j+idx]);
-            }
-            
-            dp[idx] = ans+1;
-        }
-         return dp[0];
     }
 }
