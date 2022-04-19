317c1a2cb560056504cca26aaaf2bfa8cc74834b
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-04-19 22:38:35 +0530

    Time: 2 ms (63.63%), Space: 43.3 MB (7.71%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -30,4 +30,35 @@ class Solution {
         
         return dp[idx] = ans;
     }
+    public int tabDecodeWays(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+        int ans = 0;
+        
+        for(int idx=n;idx>=0;idx--) {
+            if(idx==s.length()) {
+                dp[idx] = 1;
+                continue;
+            }   
+            if(s.charAt(idx) == '0') {
+                   dp[idx] = 0;
+                continue;
+            }
+          
+            ans = dp[idx+1];
+
+            if(idx+2<=s.length()) {
+                String sub = s.substring(idx,idx+2);
+
+                int index = Integer.parseInt(sub);
+                if(index<=26) {
+                        ans += dp[idx+2];
+                }
+            }
+
+             dp[idx] = ans;
+        }
+        
+        return dp[0];
+    }
 }
