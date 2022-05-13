7e495ba76f7d156feaee52e72e0023199f77af93
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-05-13 16:28:42 +0530

    Time: 5 ms (54.45%), Space: 45.2 MB (72.55%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -3,9 +3,34 @@ class Solution {
     List<Integer> permute = new ArrayList<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
         boolean[] vis = new boolean[nums.length];
-        Arrays.sort(nums);
-        helper(0,nums,vis);
+        //Arrays.sort(nums);
+        //helper(0,nums,vis);
+        permute2(nums,0);
         return permuteU;
+    }
+    public void permute2(int[] nums,int l) {
+        if(l==nums.length) {
+            List<Integer> subList = new ArrayList<>();
+            for(int num : nums) {
+                subList.add(num);
+            }
+            permuteU.add(subList);
+            return;
+        }
+        Set<Integer> set = new HashSet<>();
+        for(int i=l;i<nums.length;i++) {
+            if(!set.contains(nums[i])) {
+                set.add(nums[i]);
+                swap(nums,i,l);
+                permute2(nums,l+1);
+                swap(nums,i,l);
+            }
+        }
+    }
+    public void swap(int[] nums,int start,int end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
     }
     public void helper(int idx,int[] nums,boolean[] vis) {
         if(idx==nums.length) {
