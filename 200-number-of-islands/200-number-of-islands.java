eec4a84fe60e3465c2f01f0eddee81bb908e6373
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-05-15 20:50:50 +0530

    Time: 18 ms (8.51%), Space: 56.4 MB (64.02%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -13,6 +13,17 @@ class Solution {
                }
            }
        }
+       /*
+        for(int i=0;i<n;i++) {
+           for(int j=0;j<m;j++) {
+               if(!vis[i][j] && grid[i][j] == '1') {
+                 dfs(i,j,grid,vis);
+                   count++;   
+               }
+           }
+       }
+        return count;
+       */
         return count;
     }
     
