91a011da8f93ced092617fda7cd552c0e64fbca7
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-05-27 22:01:54 +0530

    Time: 59 ms (17.90%), Space: 72.7 MB (6.99%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -4,9 +4,30 @@ class Solution {
         for(int[] dp : dps) {
             Arrays.fill(dp,-1);
         }
-        int ans = helper(coins,amount,coins.length,dps);
+        int ans = helper_Tab(coins,amount,coins.length);
         
         return ans == 1e9 ? -1 : ans;
+    }
+    public int helper_Tab(int[] coins,int amt,int n) {
+        int[][] dp = new int[coins.length+1][amt+1];
+        
+        for(int i=0;i<=n;i++) {
+            for(int j=0;j<=amt;j++) {
+                if(i==0 ) {
+                    dp[i][j] = (int) 1e9;
+                    continue;
+                } if(i>0 && j==0) {
+                     dp[i][j] = 0;
+                    continue;
+                }
+                if(coins[i-1]>j) {
+                    dp[i][j] = dp[i-1][j];   
+                } else {
+                    dp[i][j] = Math.min((1 + dp[i][j - coins[i - 1]]),dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[n][amt];
     }
     public int helper(int[] coins,int amt,int n,int[][] dp) {
        if(n==0 || amt == 0) {
