0774c089a050b2f3b9a7eb6ed3603c0e5b883f6a
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-05-27 19:49:09 +0530

    Time: 2 ms (53.20%), Space: 41.5 MB (69.97%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -3,20 +3,20 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         Stack<String> sts  = new Stack<>();
         
-        String number = "";
+        int number = 0;
         StringBuilder word = new StringBuilder();
-        
+        int index = 0;
         for(int i=0;i<s.length();i++) {
             char ch = s.charAt(i);
             
             if(Character.isDigit(ch)) {
-                number = number+ch;
+                number = number * 10 + (ch - '0');
             } else if(Character.isLetter(ch)) {
                 word.append(ch);
             } else if(ch == '[') {
-                st.push(Integer.parseInt(number));
+                st.push(number);
                 sts.push(word.toString());
-                number = "";
+                number = 0;
                 word = new StringBuilder();
             } else {
                 String firstWord = sts.pop();
