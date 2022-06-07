c2342b58c273a18b1b4a6b6eb5300769c157cc22
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-06-07 14:12:42 +0530

    Time: 17 ms (60.83%), Space: 48.8 MB (29.83%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.java
+++ 126-word-ladder-ii/126-word-ladder-ii.java
@@ -1,13 +1,4 @@
 class Solution {
-    static class Edge {
-      String src;
-      String nbr;
-     
-      Edge(String src, String nbr) {
-         this.src = src;
-         this.nbr = nbr;
-      }
-    }
     List<List<String>> ladders = new ArrayList<>();
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
         if(!wordList.contains(endWord)) return ladders;
@@ -45,29 +36,6 @@ class Solution {
                 temp.remove(temp.size()-1);
             }
         }
-    }
-    public ArrayList<Edge>[] buildGraph(List<String> wordList,String beginWord) {
-        int vtces = wordList.size()+1;
-        ArrayList<Edge>[] graph = new ArrayList[vtces];
-         wordList.add(0,beginWord);
-        int length = wordList.size();
-        
-        for (int i = 0; i < vtces; i++) {
-             graph[i] = new ArrayList<>();
-        }
-       
-        for(int i=0;i<length;i++) {
-            for(int j=i+1;j<length;j++) {
-                String word1 = wordList.get(i);
-                String word2 = wordList.get(j);
-                if(isDifferenceOfOne(word1,word2)) {
-                    graph[i].add(new Edge(word1,word2));
-                    graph[j].add(new Edge(word2,word1));
-                }
-            }
-        }
-        
-        return graph;
     }
     public void buildGraph(String beginWord, Map<String, Integer> level, Map<String, Set<String>> map, Set<String> words) {
         LinkedList<String> que = new LinkedList<>();
@@ -111,66 +79,4 @@ class Solution {
         }
          return candidates;
     }
-    public boolean isDifferenceOfOne(String word1,String word2) {
-        int i = 0, j=0;
-        int diffChars = 0;
-        while(i<word1.length() && j<word2.length()) {
-            char wc1 = word1.charAt(i);
-            char wc2 = word2.charAt(j);
-            
-            if(wc1 != wc2) {
-                diffChars+= 1;
-            }
-            i++;
-            j++;
-        }
-        
-        return diffChars == 1;
-    }
-    
-   /* public void bring () {
-        LinkedList<String> que = new LinkedList<>();
-        que.addFirst(beginWord);
-        
-        
-        while(que.size()>0) {
-            int size = que.size();
-                String currWord = que.removeFirst();
-                
-                char[] wordCharArray = currWord.toCharArray();
-                for(int j=0;j<currWord.length();j++) {
-                    char temp = currWord.charAt(j); 
-                   for(char c = 'a';c<='z';c++) {
-                     
-                       if(wordCharArray[j] = c) continue;
-
-                        wordCharArray[j] = c;
-                       
-                        String newWord = new String(wordCharArray);
-
-                        if(words.contains(newWord)) {
-                            
-                            if(!visited.contains(newWord)) {
-                                
-                                visited.add(newWord);
-                                que.addLast(newWord);
-                                int nwIdx = wordList.indexOf(newWord);
-                                int currIdx = wordList.indexOf(currWord);
-                                currIdx = currIdx == -1 ? 0 : currIdx;
-                                graph[nwIdx].add(new Edge(newWord,currWord));
-                                graph[currIdx].add(new Edge(currWord,newWord));
-                            
-                            }
-                        }
-
-                        wordCharArray[j] = temp;
-                    }
-                    
-                }
-            level++;
-        }
-
-        return graph;
-    }*/
-    
 }
