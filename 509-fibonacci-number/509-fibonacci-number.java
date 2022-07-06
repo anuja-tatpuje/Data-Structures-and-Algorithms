dd2714e5537d3af40cf78d6b48f2a138f66879a9
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-07-06 19:45:55 +0530

    Time: 27 ms (5.20%), Space: 41.3 MB (22.59%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        return fib_Memoi(n,dp);
+    }
+    public int fib_Memoi(int n,int[] dp) {
+        if(n<=1) return n;
+        if(dp[n] != 0)  return dp[n];
+        
+        int ans = fib_Memoi(n-2,dp) + fib_Memoi(n-1,dp);
+        return ans;
+    }
+}
