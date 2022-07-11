0aacedce177af261264d4f2aa8676512c96d089f
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-07-11 20:52:57 +0530

    Time: 1 ms (52.70%), Space: 40.6 MB (74.58%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,12 +1,12 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+       /* int[][] dp = new int[m][n];
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
                 dp[i][j] = -1;
             }
-        }
-        int paths = getPaths(0,0,m-1,n-1,dp);
+        } */
+        int paths = getPathsTabu(m,n);
         return paths;
     }
     public static int getPaths(int sr,int sc, int dr,int dc,int[][] dp) {
@@ -22,5 +22,20 @@ class Solution {
         int verPaths = getPaths(sr+1,sc,dr,dc,dp);
         
         return dp[sr][sc] = horPaths+verPaths;
+    }
+    public static int getPathsTabu(int m,int n) {
+        int[][] dp = new int[m][n];
+        
+        for(int i=m-1;i>=0;i--) {
+            for(int j=n-1;j>=0;j--) {
+                if(i==m-1 || j== n-1) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+                }
+            }
+        }
+        
+        return dp[0][0];
     }
 }
