63e77cd643ea5412e938025363daa4d43de95136
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-07-27 21:21:02 +0530

    Time: 3 ms (45.01%), Space: 43.5 MB (66.71%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<String> para = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        generatePara(0,0,n,"");
+        return para;
+    }
+    public void generatePara(int open,int close,int max,String par) {
+        if(par.length() ==  max * 2) {
+            para.add(par);
+            return;
+        }
+        if(open<max) {
+            generatePara(open+1,close,max,par+"(");
+        }
+        if(close < open) {
+            generatePara(open,close+1,max,par+")");
+        }
+    }
+}
