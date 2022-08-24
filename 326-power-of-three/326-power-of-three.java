b5b5962393ef10c84dc38aac673a9e2123bf3c9d
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-08-24 19:58:06 +0530

    Time: 30 ms (16.58%), Space: 47.7 MB (20.27%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        
+        while(n>1) {
+            if(n % 3 != 0) return false;
+            
+            n/=3;
+        }
+        
+        return n>0;
+    }
+}
