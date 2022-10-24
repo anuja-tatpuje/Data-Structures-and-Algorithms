d0aabe55785b17ba52b0d36a5e175fc579e56934
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-10-24 19:23:26 +0530

    Time: 835 ms (7.42%), Space: 69.8 MB (6.85%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean canJump(int[] nums) {
+    public boolean canJumpGreedy(int[] nums) {
        int n = nums.length;
         int maxJump = 0;
         for(int i=0;i<n;i++) {
@@ -11,6 +11,33 @@ class Solution {
         }
         
         return true;
+    }
+    public boolean canJump(int[] nums) {
+      int[] dp = new int[nums.length];
+      Arrays.fill(dp, -1);
+       return helper(0,nums,dp);
+    }
+    public boolean helper(int index,int[] nums,int[] dp) {
+        if(index >= nums.length-1) {
+            return true;
+        }
+        
+        if(nums[index] == 0) {
+            dp[index] = 0;
+            return false;
+        }
+        
+        if(dp[index] != -1) return dp[index] == 1;
+        
+        int jumpsAllowed = nums[index];
+        for(int j=1;j<=jumpsAllowed;j++) {
+            if(helper(index+j,nums,dp)) {
+                dp[index] = 1;
+                return true;
+            }
+        }
+        dp[index] = 0;
+        return false;
     }
    
 }
