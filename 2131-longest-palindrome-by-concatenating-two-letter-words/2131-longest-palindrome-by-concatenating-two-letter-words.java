f4afe47d744e7bc0ffd404845fb969c3bdc54453
Anuja Tatpuje <99488192+anuja-tatpuje@users.noreply.github.com>
2022-11-10 21:59:16 +0530

    Time: 527 ms (5.09%), Space: 124.6 MB (20.72%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,59 +1,28 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-      //  HashMap<String,Integer> map = new HashMap<>();
+        HashMap<String,Integer> map = new HashMap<>();
        
-      /*  int ans = 0, unpaired = 0;
+        int ans = 0;
         
         for(String word : words) {
-            if(word.charAt(1) == word.charAt(0)) {
-                if(map.containsKey(word) && map.get(word) > 0) {
-                    unpaired--;
-                    map.put(word,map.get(word)-1);
-                    ans += 4;
-                } else {
-                    unpaired++;
-                    map.put(word,map.getOrDefault(word,0)+1);
-                }
-            } else {
-                String rev = ""+word.charAt(1)+word.charAt(0);
-                if(map.containsKey(rev) && map.get(rev) > 0) {
-                    ans += 4;
-                    map.put(word,map.get(rev)-1);
-                } else {
-                    map.put(word,map.getOrDefault(word,0)+1);
-                }
+            String rev = ""+word.charAt(1) + word.charAt(0);
+            if(map.containsKey(rev)) {
+                ans += 4;
+                map.put(rev,map.get(rev)-1);
+                if(map.get(rev) == 0) map.remove(rev);
+                continue;
             }
-        }
-        System.out.println(ans);
-        if(unpaired>0) ans += 2; */
+            map.put(word,map.getOrDefault(word,0)+1);
+        } 
         
-        HashMap<String, Integer> m = new HashMap();
-    int unpaired = 0, ans = 0;
-    for (String w: words) {
-        if (!m.containsKey(w)) m.put(w, 0);
-        if (w.charAt(0) == w.charAt(1)) {
-            if (m.get(w) > 0) {
-                unpaired--;
-                m.put(w, m.get(w) - 1);
-                ans += 4;
-            }
-            else {
-                m.put(w, m.get(w) + 1);
-                unpaired++;
+        for(String word : map.keySet()) {
+            if(map.get(word) == 1 && word.charAt(1) == word.charAt(0)) {
+                ans += 2;
+                break;
             }
         }
-        else {
-            String rev = Character.toString(w.charAt(1)) + 
-                Character.toString(w.charAt(0));
-            if (m.containsKey(rev) && m.get(rev) > 0) {
-                ans += 4;
-                m.put(rev, m.get(rev) - 1);
-            }
-            else m.put(w, m.get(w) + 1);
-        }
-
-    }
-    if (unpaired > 0) ans += 2;
+        
+        System.out.println(ans);
         return ans;
     }
 }
